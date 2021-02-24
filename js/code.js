c1ca15cf744603a6ae35d6a66999bf703e41c687
Matthew Di Micelli <mrd2689a@gmail.com>
2021-02-24 12:01:50 -0500

    added button to clear the grid, prompt user for size, then change grid to that size. hovering on squares changes then to random color.  size of screen is now consistent no matter the size of the grid.

--- js/code.js
+++ js/code.js
@@ -5,18 +5,53 @@ function createDivwithID(id) {
   return div;
 }
 
+let numofDivs = 256;
 const container = document.querySelector("#container");
-createAndAppend256Divs();
-const squares = document.querySelectorAll(".square");
-squares.forEach((square) => {
-  square.addEventListener("mouseenter", () => {
-    square.classList.toggle("hovered");
-  });
+appendDivs(numofDivs);
+changeColorUponHovering();
+const button = document.createElement("button");
+button.textContent = "Click Me";
+button.setAttribute("id", "mainButton");
+const body = document.querySelector("body");
+body.insertBefore(button, container);
+button.addEventListener("click", () => {
+  clearScreen();
+  setSizeofSidedofGrid(getDesiredSizeofGrid());
+  changeColorUponHovering();
 });
 
-function createAndAppend256Divs() {
-  for (let i = 1; i <= 256; i++) {
+function changeColorUponHovering() {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.addEventListener("mouseenter", () => {
+      square.style.backgroundColor =
+        "#" + Math.floor(Math.random() * 16777215).toString(16);
+    });
+  });
+}
+
+function appendDivs(x) {
+  for (let i = 1; i <= x; i++) {
     const div = createDivwithID(i);
     container.appendChild(div);
   }
+}
+
+function clearScreen() {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    container.removeChild(square);
+  });
+}
+
+function getDesiredSizeofGrid() {
+  let size = prompt("How many squares should each side of the grid be?");
+  return parseInt(size);
+}
+
+function setSizeofSidedofGrid(x) {
+  container.style["grid-template-columns"] = `repeat(${x}, 1fr)`;
+  container.style["grid-template-rows"] = `repeat(${x}, 1fr)`;
+  //Calls appendDivs(), which in turn calls function to create the divs (x**2 divs)
+  appendDivs(x * x);
 }
