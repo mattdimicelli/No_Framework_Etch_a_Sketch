0913187fe43976d27f2ef3deda9cb8cea541d556
Matt Di Micelli <matthewdimicelli@gmail.com>
2021-12-11 20:48:58 -0500

    refactor

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function createGrid(numberOfCellsPerAxis) {
   for (let i = 0; i < numberOfCellsPerAxis ** 2; i++) {
     /* the numberOfCellsPerAxis is squared to get the total number of
     cells for the grid */
-    container.append(createCell(i));
+    container.append(createCell());
   }
   container.style.gridTemplateColumns = `repeat(${numberOfCellsPerAxis}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${numberOfCellsPerAxis}, 1fr)`;
@@ -26,8 +26,26 @@ function createGrid(numberOfCellsPerAxis) {
 
 function changeColor(e) {
   let cell = e.currentTarget;
-  cell.style.backgroundColor =
-      "#" + Math.floor(Math.random() * 16777215).toString(16);
+  if ('shade' in cell) {
+    /* cell has been previously shaded darker on a spectrum leading to black.
+    continue to shade the cell darker until it is black */
+    
+  } else if (!('shade' in cell)) {
+    console.log(getComputedStyle(cell).backgroundColor);
+    const REGEX = /rgb\((?:([0-9]{1,2}|1[0-9]{1,2}|2[0-4][0-9]|25[0-5]), ?)(?:([0-9]{1,2}|1[0-9]{1,2}|2[0-4][0-9]|25[0-5]), ?)(?:([0-9]{1,2}|1[0-9]{1,2}|2[0-4][0-9]|25[0-5]))\)/;
+    const cellRGB = getComputedStyle(cell).backgroundColor.match(REGEX);
+    const r = +cellRGB[1];
+    const g = +cellRGB[2];
+    const b = +cellRGB[3];
+    const lightness = rgbToLightness(r,g,b);
+    console.log(lightness);
+    
+    function rgbToLightness(r,g,b) {
+      return 1/2 * (Math.max(r,g,b) + Math.min(r,g,b));
+    } 
+  }
+  // cell.style.backgroundColor =
+  //     "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
 function btnClickHandler() {
